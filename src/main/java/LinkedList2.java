c477a9ed733f9ef7252639b751efdbffbaab5be1
Graur <ancongr@yandex.ru>
2020-10-04 22:35:48 +0300

    add equals and hashcode to Node class

--- src/main/java/LinkedList2.java
+++ src/main/java/LinkedList2.java
@@ -148,4 +148,19 @@ class Node
         next = null;
         prev = null;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return value == node.value &&
+                Objects.equals(next, node.next) &&
+                Objects.equals(prev, node.prev);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, next, prev);
+    }
 }
