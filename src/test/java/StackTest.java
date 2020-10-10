7834eefe63a8d1f9811e9b1adae76327c194272e
Graur <ancongr@yandex.ru>
2020-10-10 16:35:28 +0300

    task 4: Stack (origin template)

---
+++ src/test/java/StackTest.java
@@ -0,0 +1,20 @@
+import junit.framework.TestCase;
+
+public class StackTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testSize() {
+    }
+
+    public void testPop() {
+    }
+
+    public void testPush() {
+    }
+
+    public void testPeek() {
+    }
+}
