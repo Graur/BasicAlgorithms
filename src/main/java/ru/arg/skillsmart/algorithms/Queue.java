49f41267ef1d7df6b593440b6f3b17310981720c
Graur <ancongr@yandex.ru>
2020-10-11 22:01:46 +0300

    add my realization of Queue (founds on LinkedList)

--- src/main/java/ru/arg/skillsmart/algorithms/Queue.java
+++ src/main/java/ru/arg/skillsmart/algorithms/Queue.java
@@ -4,25 +4,34 @@ import java.util.*;
 
 public class Queue<T>
 {
+
+    private LinkedList<T> array;
+
     public Queue()
     {
         // инициализация внутреннего хранилища очереди
+        array = new LinkedList<>();
     }
 
     public void enqueue(T item)
     {
         // вставка в хвост
+        array.add(item);
     }
 
     public T dequeue()
     {
         // выдача из головы
-        return null; // null если очередь пустая
+        if (array.size() == 0) {
+            return null; // null если очередь пустая
+        } else {
+            return array.poll();
+        }
     }
 
     public int size()
     {
-        return 0; // размер очереди
+        return array.size();
     }
 
 }
--- src/test/java/ru/arg/skillsmart/algorithms/QueueTest.java
+++ src/test/java/ru/arg/skillsmart/algorithms/QueueTest.java
@@ -1,19 +1,52 @@
 package ru.arg.skillsmart.algorithms;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class QueueTest extends TestCase {
 
-    public void setUp() throws Exception {
-        super.setUp();
+    private Queue<Integer> queue;
+
+    public void setUp() {
+        queue = new Queue<>();
     }
 
+    @Test
     public void testEnqueue() {
+        setUp();
+        assertEquals(0, queue.size());
+        queue.enqueue(1);
+        assertEquals(1, queue.size());
+        queue.enqueue(2);
+        assertEquals(2, queue.size());
     }
 
+    @Test
     public void testDequeue() {
+        setUp();
+        queue.enqueue(1);
+        queue.enqueue(2);
+
+        Integer dequeue = queue.dequeue();
+        assertEquals(Integer.valueOf(1), dequeue);
+        assertEquals(1, queue.size());
+        Integer dequeue2 = queue.dequeue();
+        assertEquals(Integer.valueOf(2), dequeue2);
+        assertEquals(0, queue.size());
+        Integer dequeue3 = queue.dequeue();
+        assertNull(dequeue3);
     }
 
+    @Test
     public void testSize() {
+        setUp();
+        assertEquals(0, queue.size());
+        queue.enqueue(1);
+        assertEquals(1, queue.size());
+        queue.enqueue(2);
+        assertEquals(2, queue.size());
     }
 }
