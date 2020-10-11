735f4c1ccb5c7970a6a2b6e82822c7a6ae57e4ac
Graur <ancongr@yandex.ru>
2020-10-11 22:21:14 +0300

    Queue by 2 stacks

--- src/main/java/ru/arg/skillsmart/algorithms/Queue.java
+++ src/main/java/ru/arg/skillsmart/algorithms/Queue.java
@@ -1,37 +1,45 @@
 package ru.arg.skillsmart.algorithms;
 
-import java.util.*;
-
 public class Queue<T>
 {
 
-    private LinkedList<T> array;
+    private Stack<T> stack;
+    private Stack<T> buffer;
+    private int size;
 
     public Queue()
     {
         // инициализация внутреннего хранилища очереди
-        array = new LinkedList<>();
+        stack = new Stack<>();
+        buffer = new Stack<>();
     }
 
     public void enqueue(T item)
     {
         // вставка в хвост
-        array.add(item);
+        stack.push(item);
+        size++;
     }
 
     public T dequeue()
     {
         // выдача из головы
-        if (array.size() == 0) {
+        if (size == 0) {
             return null; // null если очередь пустая
         } else {
-            return array.poll();
+            if (buffer.size() == 0) {
+                while (stack.size() != 0) {
+                    buffer.push(stack.pop());
+                }
+            }
+            size--;
+            return buffer.pop();
         }
     }
 
     public int size()
     {
-        return array.size();
+        return size;
     }
 
 }
