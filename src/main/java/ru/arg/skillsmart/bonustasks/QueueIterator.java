58a4e60ba8699d0d92e1211fcd1448e792259bf7
Graur <ancongr@yandex.ru>
2020-10-12 18:32:57 +0300

    refactor Queue iterator

--- src/main/java/ru/arg/skillsmart/bonustasks/QueueIterator.java
+++ src/main/java/ru/arg/skillsmart/bonustasks/QueueIterator.java
@@ -4,18 +4,9 @@ import ru.arg.skillsmart.algorithms.Queue;
 
 public class QueueIterator<T> {
     public T iterate(Queue<T> queue, int n) {
-        T result = null;
-        if (n == 0 || queue.size() == 0) {
-            return null;
+        for (int i = 1; i < n; i++) {
+            queue.enqueue(queue.dequeue());
         }
-
-        for (int i = 1; i <= n; i++) {
-            if (i == n) {
-                result = queue.dequeue();
-            } else {
-                queue.enqueue(queue.dequeue());
-            }
-        }
-        return result;
+        return queue.dequeue();
     }
 }
