577e173c787b8253fa5e3060e036862048fa2822
Graur <ancongr@yandex.ru>
2020-10-12 20:50:31 +0300

    Deque realization

---
+++ src/main/java/ru/arg/skillsmart/algorithms/Deque.java
@@ -0,0 +1,115 @@
+package ru.arg.skillsmart.algorithms;
+import java.util.*;
+
+public class Deque<T>
+{
+    public Node<T> head;
+    public Node<T> tail;
+    private int size;
+
+    public Deque()
+    {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public void addFront(T item)
+    {
+        // добавление в голову
+        Node<T> oldHead = this.head;
+        Node<T> newHead = new Node<>(null, item,oldHead);
+        this.head = newHead;
+        if (oldHead == null) {
+            this.tail = newHead;
+        } else {
+            oldHead.prev = newHead;
+        }
+        size++;
+    }
+
+    public void addTail(T item)
+    {
+        // добавление в хвост
+        Node<T> oldTail = this.tail;
+        Node<T> newTail = new Node<>(oldTail, item,null);
+        this.tail = newTail;
+        if (oldTail == null) {
+            this.head = newTail;
+        } else {
+            oldTail.next = newTail;
+        }
+        size++;
+    }
+
+    public T removeFront()
+    {
+        // удаление из головы
+        Node<T> deletedEl = this.head;
+        Node<T> newHead = this.head.next;
+        this.head = newHead;
+        if (newHead == null) {
+            this.tail = null;
+        } else {
+            newHead.prev = null;
+        }
+        size--;
+        return deletedEl.value;
+    }
+
+    public T removeTail()
+    {
+        // удаление из хвоста
+        Node<T> deletedEl = this.tail;
+        Node<T> newTail = this.tail.prev;
+        this.tail = newTail;
+        if (newTail == null) {
+            this.head = null;
+        } else {
+            newTail.next = null;
+        }
+        size--;
+        return deletedEl.value;
+    }
+
+    public int size()
+    {
+        return size;
+    }
+}
+
+class Node<T>
+{
+    public T value;
+    public Node<T> next;
+    public Node<T> prev;
+
+    public Node(T _value)
+    {
+        this.value = _value;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public Node(Node<T> prev, T _value, Node<T> next)
+    {
+        this.value = _value;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?> node = (Node<?>) o;
+        return Objects.equals(value, node.value) &&
+                Objects.equals(next, node.next) &&
+                Objects.equals(prev, node.prev);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, next, prev);
+    }
+}
---
+++ src/test/java/ru/arg/skillsmart/algorithms/DequeTest.java
@@ -0,0 +1,91 @@
+package ru.arg.skillsmart.algorithms;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class DequeTest extends TestCase {
+
+    private Deque<Integer> deque;
+
+    public void setUp() {
+        deque = new Deque<>();
+        deque.addTail(1);
+        deque.addTail(2);
+        deque.addTail(3);
+        deque.addTail(4);
+        deque.addTail(5);
+    }
+
+    @Test
+    public void testAddFront() {
+        setUp();
+        assertEquals(5, deque.size());
+        deque.addFront(-1);
+        assertNull(deque.head.prev);
+        assertEquals(Integer.valueOf(5), deque.tail.value);
+        assertEquals(Integer.valueOf(-1), deque.head.value);
+        assertEquals(Integer.valueOf(1), deque.head.next.value);
+        assertEquals(Integer.valueOf(2), deque.head.next.next.value);
+        assertEquals(6, deque.size());
+    }
+
+    @Test
+    public void testAddTail() {
+        setUp();
+        assertEquals(5, deque.size());
+        deque.addTail(6);
+        assertNull(deque.tail.next);
+        assertEquals(Integer.valueOf(1), deque.head.value);
+        assertEquals(Integer.valueOf(6), deque.tail.value);
+        assertEquals(Integer.valueOf(5), deque.tail.prev.value);
+        assertEquals(Integer.valueOf(4), deque.tail.prev.prev.value);
+        assertEquals(6, deque.size());
+    }
+
+    @Test
+    public void testRemoveFront() {
+        setUp();
+        assertEquals(5, deque.size());
+        Integer integer = deque.removeFront();
+        assertEquals(4, deque.size());
+        assertNull(deque.tail.next);
+        assertNull(deque.head.prev);
+        assertEquals(Integer.valueOf(1), integer);
+        assertEquals(Integer.valueOf(2), deque.head.value);
+        assertEquals(Integer.valueOf(3), deque.head.next.value);
+        deque.removeFront();
+        deque.removeFront();
+        deque.removeFront();
+        deque.removeFront();
+        assertNull(deque.head);
+        assertNull(deque.tail);
+    }
+
+    @Test
+    public void testRemoveTail() {
+        setUp();
+        assertEquals(5, deque.size());
+        Integer integer = deque.removeTail();
+        assertEquals(4, deque.size());
+        assertNull(deque.tail.next);
+        assertNull(deque.head.prev);
+        assertEquals(Integer.valueOf(5), integer);
+        assertEquals(Integer.valueOf(4), deque.tail.value);
+        assertEquals(Integer.valueOf(3), deque.tail.prev.value);
+        deque.removeTail();
+        deque.removeTail();
+        deque.removeTail();
+        deque.removeTail();
+        assertNull(deque.head);
+        assertNull(deque.tail);
+    }
+
+    @Test
+    public void testSize() {
+        setUp();
+        assertEquals(5, deque.size());
+    }
+}
