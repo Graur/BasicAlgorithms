d749d0a76be5213c0ab22e19515a325f7540699c
Graur <ancongr@yandex.ru>
2020-10-18 15:00:22 +0300

    Task 8: Hashtable

---
+++ src/test/java/ru/arg/skillsmart/algorithms/HashTableTest.java
@@ -0,0 +1,29 @@
+package ru.arg.skillsmart.algorithms;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HashTableTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void hashFun() {
+    }
+
+    @Test
+    public void seekSlot() {
+    }
+
+    @Test
+    public void put() {
+    }
+
+    @Test
+    public void find() {
+    }
+}
