7f794affc6861ae6ea7348d34fd982805ca40a70
Graur <ancongr@yandex.ru>
2020-10-18 22:07:47 +0300

    Task 9: HashMap (done)

--- src/main/java/ru/arg/skillsmart/algorithms/NativeDictionary.java
+++ src/main/java/ru/arg/skillsmart/algorithms/NativeDictionary.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 class NativeDictionary<T>
 {
     public int size;
+    private int step;
     public String [] slots;
     public T [] values;
 
@@ -12,31 +13,82 @@ class NativeDictionary<T>
         size = sz;
         slots = new String[size];
         values = (T[]) Array.newInstance(clazz, this.size);
+        step = 3;
     }
 
     public int hashFun(String key)
     {
         // всегда возвращает корректный индекс слота
-        return 0;
+//        int h;
+//        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+        int h = 0;
+        byte[] bytes = key.getBytes();
+        for (byte v : bytes) {
+            h += v;
+        }
+        return h % size;
+    }
+
+    private int seekSlot(String key)
+    {
+        // находит индекс пустого слота для значения, или -1
+        int index = hashFun(key);
+        if (slots[index] == null || slots[index].equals(key)) {
+            return index;
+        } else {
+            int count = 0;
+            int newIndex = index + step >= size ? step - (size - index) : index + step;
+            while (count != size) {
+                if (slots[newIndex] == null)  {
+                    return newIndex;
+                } else {
+                    newIndex = newIndex + step >= size ? step - (size - newIndex) : newIndex + step;
+                    count++;
+                }
+            }
+        }
+        return -1;
     }
 
     public boolean isKey(String key)
     {
         // возвращает true если ключ имеется,
         // иначе false
-        return false;
+       return get(key) != null;
     }
 
     public void put(String key, T value)
     {
         // гарантированно записываем
         // значение value по ключу key
+        int index = seekSlot(key);
+        if (index != -1) {
+            slots[index] = key;
+            values[index] = value;
+        }
     }
 
     public T get(String key)
     {
         // возвращает value для key,
         // или null если ключ не найден
+        int index = hashFun(key);
+        if (slots[index] == null) {
+            return null;
+        } else if (slots[index].equals(key)) {
+            return values[index];
+        } else {
+            int count = 0;
+            int newIndex = index + step >= size ? step - (size - index) : index + step;
+            while (count != size) {
+                if (slots[newIndex] != null && slots[newIndex].equals(key))  {
+                    return values[newIndex];
+                } else {
+                    newIndex = newIndex + step >= size ? step - (size - newIndex) : newIndex + step;
+                    count++;
+                }
+            }
+        }
         return null;
     }
 }
--- src/test/java/ru/arg/skillsmart/algorithms/NativeDictionaryTest.java
+++ src/test/java/ru/arg/skillsmart/algorithms/NativeDictionaryTest.java
@@ -5,28 +5,73 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class NativeDictionaryTest {
 
-    @Before
-    public void setUp() throws Exception {
+    private NativeDictionary<Integer> simple;
+
+    private NativeDictionary<Integer> collision;
+
+    public void setUp() {
+        simple = new NativeDictionary<>(11, Integer.class);
+        simple.put("1", 1);
+        simple.put("2", 2);
+        collision = new NativeDictionary<>(11, Integer.class);
+        collision.put("1", 1);
+        collision.put("2", 2);
     }
 
     @Test
     public void hashFun() {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("1", 1);
+        map.put("1", 2);
+        map.entrySet().forEach(e -> {
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        });
     }
 
     @Test
     public void isKey() {
+        setUp();
+        simple.put("3", 3);
+        assertTrue(simple.isKey("3"));
+        assertFalse(simple.isKey("asdfasdgasgsg"));
+        collision.put("ab", 100);
+        collision.put("ba", 200);
+        assertTrue(collision.isKey("ab"));
+        assertTrue(collision.isKey("ba"));
+        assertFalse(collision.isKey("fasfasdfasf"));
     }
 
     @Test
     public void put() {
+        setUp();
+        simple.put("3", 3);
+        assertEquals(Integer.valueOf(3), simple.get("3"));
+        collision.put("ab", 100);
+        collision.put("ba", 200);
+        assertEquals(Integer.valueOf(100), collision.get("ab"));
+        assertEquals(Integer.valueOf(200), collision.get("ba"));
+        collision.put("1", 777);
+        assertEquals(Integer.valueOf(777), collision.get("1"));
     }
 
     @Test
     public void get() {
+        setUp();
+        simple.put("3", 3);
+        assertEquals(Integer.valueOf(3), simple.get("3"));
+        collision.put("ab", 100);
+        collision.put("ba", 200);
+        assertEquals(Integer.valueOf(100), collision.get("ab"));
+        assertEquals(Integer.valueOf(200), collision.get("ba"));
+        assertNull(simple.get("sdfasdfsffSF"));
+        assertNull(collision.get("sdfasdfsffSF"));
     }
 }
