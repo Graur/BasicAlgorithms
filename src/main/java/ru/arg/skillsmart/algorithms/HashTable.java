1533c99c8b0eda043caf38b03858cfb1aa3424ab
Graur <ancongr@yandex.ru>
2020-10-18 16:41:24 +0300

    Task 8: Hashtable resolved

--- src/main/java/ru/arg/skillsmart/algorithms/HashTable.java
+++ src/main/java/ru/arg/skillsmart/algorithms/HashTable.java
@@ -4,7 +4,7 @@ public class HashTable
 {
     public int size;
     public int step;
-    public String [] slots;
+    public String[] slots;
 
     public HashTable(int sz, int stp)
     {
@@ -17,27 +17,54 @@ public class HashTable
     public int hashFun(String value)
     {
         // всегда возвращает корректный индекс слота
-        return 0;
+        int h = 0;
+        byte[] bytes = value.getBytes();
+        for (byte v : bytes) {
+            h += v;
+        }
+        return h % size;
     }
 
     public int seekSlot(String value)
     {
         // находит индекс пустого слота для значения, или -1
+        int index = hashFun(value);
+        if (slots[index] == null) {
+            return index;
+        } else {
+            int count = 0;
+            int newIndex = index + step >= size ? step - (size - index) : index + step;
+            while (count != size) {
+                if (slots[newIndex] == null)  {
+                    return newIndex;
+                } else {
+                    newIndex = newIndex + step >= size ? step - (size - newIndex) : newIndex + step;
+                    count++;
+                }
+            }
+        }
         return -1;
     }
 
     public int put(String value)
     {
         // записываем значение по хэш-функции
-
+        int index = seekSlot(value);
         // возвращается индекс слота или -1
         // если из-за коллизий элемент не удаётся разместить
-        return -1;
+        if (index != -1) {
+            slots[index] = value;
+        }
+        return index;
     }
 
     public int find(String value)
     {
-        // находит индекс слота со значением, или -1
-        return -1;
+        int index = hashFun(value);
+        if (slots[index].equals(value)) {
+            return index;
+        } else {
+            return -1;
+        }
     }
 }
--- src/test/java/ru/arg/skillsmart/algorithms/HashTableTest.java
+++ src/test/java/ru/arg/skillsmart/algorithms/HashTableTest.java
@@ -2,28 +2,114 @@ package ru.arg.skillsmart.algorithms;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
+@RunWith(JUnit4.class)
 public class HashTableTest {
 
-    @Before
-    public void setUp() throws Exception {
+    private HashTable simpleTable;
+
+    private HashTable collisionTable;
+
+    public void setUp() {
+        simpleTable = new HashTable(13, 3);
+        simpleTable.put("1");
+        simpleTable.put("2");
+        simpleTable.put("3");
+        simpleTable.put("4");
+        simpleTable.put("5");
+        simpleTable.put("6");
+        simpleTable.put("7");
+        simpleTable.put("8");
+        simpleTable.put("9");
+        simpleTable.put("10");
+        simpleTable.put("11");
+        simpleTable.put("12");
+        simpleTable.put("13");
+
+        collisionTable = new HashTable(11, 3);
+        collisionTable.put("1");
+        collisionTable.put("2");
+        collisionTable.put("3");
+        collisionTable.put("4");
+        collisionTable.put("5");
     }
 
     @Test
     public void hashFun() {
+        simpleTable = new HashTable(17, 3);
+        System.out.println(simpleTable.hashFun("98"));
     }
 
     @Test
     public void seekSlot() {
+        setUp();
+        assertEquals(13, simpleTable.size);
+        assertEquals(-1, simpleTable.put("zzz"));
+        System.out.println("SIMPLE TABLE");
+        for (String s : simpleTable.slots) {
+            System.out.println(s);
+        }
+
+        System.out.println("COLLISION TABLE");
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertEquals(-1, collisionTable.put("1"));
+
+        System.out.println("============================");
+        for (String s : collisionTable.slots) {
+            System.out.println(s);
+        }
     }
 
     @Test
     public void put() {
+        setUp();
+        assertEquals(13, simpleTable.size);
+        assertEquals(-1, simpleTable.put("zzz"));
+        System.out.println("SIMPLE TABLE");
+        for (String s : simpleTable.slots) {
+            System.out.println(s);
+        }
+
+        System.out.println("COLLISION TABLE");
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertNotEquals(-1, collisionTable.put("1"));
+        assertEquals(-1, collisionTable.put("1"));
+
+        System.out.println("============================");
+        for (String s : collisionTable.slots) {
+            System.out.println(s);
+        }
     }
 
     @Test
     public void find() {
+        setUp();
+        int simpleIndex = simpleTable.find("1");
+        assertNotEquals(-1, simpleIndex);
+        assertEquals(simpleTable.slots[simpleIndex], "1");
+        assertEquals(-1, simpleTable.find("ffffff"));
+        int collusionIndex = collisionTable.find("1");
+        assertNotEquals(-1, collusionIndex);
+        assertEquals(collisionTable.slots[collusionIndex], "1");
+        collisionTable.put("1");
+        int collusionIndex2 = collisionTable.find("1");
+        assertEquals(collisionTable.slots[collusionIndex2], "1");
+        assertEquals(-1, collisionTable.find("ffffff"));
     }
 }
