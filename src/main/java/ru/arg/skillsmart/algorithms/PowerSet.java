44fd1cbb3b051b380f95c588c3c00ae346f72331
Graur <ancongr@yandex.ru>
2020-10-19 22:08:40 +0300

    Task 10: HashSet (done)

--- src/main/java/ru/arg/skillsmart/algorithms/PowerSet.java
+++ src/main/java/ru/arg/skillsmart/algorithms/PowerSet.java
@@ -1,55 +1,189 @@
 package ru.arg.skillsmart.algorithms;
 
+import java.util.Arrays;
+
 public class PowerSet
 {
+    public int size;
+    public int step;
+    public String[] slots;
+    public int count;
 
     public PowerSet()
     {
         // ваша реализация хранилища
+        size = 20000;
+        step = 3;
+        slots = new String[size];
+        for(int i=0; i<size; i++) slots[i] = null;
+    }
+
+    public PowerSet(int sz)
+    {
+        // ваша реализация хранилища
+        size = sz;
+        step = 3;
+        slots = new String[size];
+        for(int i=0; i<size; i++) slots[i] = null;
     }
 
     public int size()
     {
         // количество элементов в множестве
-        return 0;
+        return count;
     }
 
+    public int hashFun(String value)
+    {
+        // всегда возвращает корректный индекс слота
+        int h = 0;
+        byte[] bytes = value.getBytes();
+        for (byte v : bytes) {
+            h += v;
+        }
+        return h % size;
+    }
+
+    private int seekSlot(String key)
+    {
+        // находит индекс пустого слота для значения, или -1
+        int index = hashFun(key);
+        if (slots[index] == null || slots[index].equals(key)) {
+            return index;
+        } else {
+            int cnt = 0;
+            int newIndex = index + step >= size ? step - (size - index) : index + step;
+            while (cnt != size) {
+                if (slots[newIndex] == null) {
+                    return newIndex;
+                } else {
+                    newIndex = newIndex + step >= size ? step - (size - newIndex) : newIndex + step;
+                    cnt++;
+                }
+            }
+        }
+        return -1;
+    }
 
     public void put(String value)
     {
         // всегда срабатывает
+        if (get(value)) {
+            return;
+        }
+        int index = seekSlot(value);
+        if (index != -1) {
+            slots[index] = value;
+            count++;
+        }
     }
 
     public boolean get(String value)
     {
         // возвращает true если value имеется в множестве,
         // иначе false
+        int index = hashFun(value);
+        if (slots[index] == null) {
+            return false;
+        } else if (slots[index].equals(value)) {
+            return true;
+        } else {
+            int count = 0;
+            int newIndex = index + step >= size ? step - (size - index) : index + step;
+            while (count != size) {
+                if (slots[newIndex] != null && slots[newIndex].equals(value))  {
+                    return true;
+                } else {
+                    newIndex = newIndex + step >= size ? step - (size - newIndex) : newIndex + step;
+                    count++;
+                }
+            }
+        }
         return false;
+    }
+
+    public int find(String value)
+    {
+        if (value == null) {
+            return -1;
+        }
+        int index = hashFun(value);
+        if (slots[index].equals(value)) {
+            return index;
+        } else {
+            int count = 0;
+            int newIndex = index + step >= size ? step - (size - index) : index + step;
+            while (count != size) {
+                if (slots[newIndex] != null && slots[newIndex].equals(value))  {
+                    return newIndex;
+                } else {
+                    newIndex = newIndex + step >= size ? step - (size - newIndex) : newIndex + step;
+                    count++;
+                }
+            }
+        }
+        return -1;
     }
 
     public boolean remove(String value)
     {
         // возвращает true если value удалено
         // иначе false
+        if (!get(value)) {
+            return false;
+        }
+        int index = find(value);
+        if (index != -1 && slots[index] != null) {
+            slots[index] = null;
+            count--;
+            return true;
+        }
         return false;
     }
 
     public PowerSet intersection(PowerSet set2)
     {
         // пересечение текущего множества и set2
-        return null;
+        if (set2 == null) {
+            return new PowerSet();
+        }
+        PowerSet result = new PowerSet();
+        for (int i = 0; i < set2.size; i++) {
+            if (set2.slots[i] != null && get(set2.slots[i])) {
+                result.put(set2.slots[i]);
+            }
+        }
+        return result;
     }
 
     public PowerSet union(PowerSet set2)
     {
         // объединение текущего множества и set2
-        return null;
+        PowerSet result = new PowerSet(size() + set2.size());
+        for (int i = 0; i < size; i++) {
+            if (slots[i] != null) {
+                result.put(slots[i]);
+            }
+        }
+
+        for (int i = 0; i < set2.size; i++) {
+            if (set2.slots[i] != null) {
+                result.put(set2.slots[i]);
+            }
+        }
+        return result;
     }
 
     public PowerSet difference(PowerSet set2)
     {
         // разница текущего множества и set2
-        return null;
+        PowerSet result = new PowerSet();
+        for (int i = 0; i < size; i++) {
+            if (slots[i] != null && !set2.get(slots[i])) {
+                result.put(slots[i]);
+            }
+        }
+        return result;
     }
 
     public boolean isSubset(PowerSet set2)
@@ -57,6 +191,11 @@ public class PowerSet
         // возвращает true, если set2 есть
         // подмножество текущего множества,
         // иначе false
-        return false;
+        for (int i = 0; i < set2.size; i++) {
+            if (set2.slots[i] != null && !get(set2.slots[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/ru/arg/skillsmart/algorithms/PowerSetTest.java
+++ src/test/java/ru/arg/skillsmart/algorithms/PowerSetTest.java
@@ -1,6 +1,5 @@
 package ru.arg.skillsmart.algorithms;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -10,38 +9,245 @@ import static org.junit.Assert.*;
 @RunWith(JUnit4.class)
 public class PowerSetTest {
 
+    public static final int numbersOfSize = 18000;
+
+    private PowerSet powerSet;
+
+    private PowerSet powerSet2;
+
+    private PowerSet powerSet3;
+
+    private PowerSet powerSet4;
+
     public void setUp() {
+        powerSet = new PowerSet();
+        for (int i = 0; i < numbersOfSize; i++) {
+            powerSet.put(Integer.toString(i));
+        }
+    }
+
+    public void setUp2() {
+        powerSet2 = new PowerSet();
+        for (int i = numbersOfSize; i < numbersOfSize + 15000; i++) {
+            powerSet2.put("abcvbdgdrefsdf" + i);
+        }
+    }
+
+    public void setUp3() {
+        powerSet3 = new PowerSet();
+        for (int i = numbersOfSize; i < numbersOfSize + 10000; i++) {
+            powerSet3.put("asdfasdf" + i);
+        }
+
+        for (int i = 0; i < numbersOfSize - 17000; i++) {
+            powerSet3.put(Integer.toString(i));
+        }
+    }
+
+    public void setUp4() {
+        powerSet4 = new PowerSet();
+
+        for (int i = 0; i < numbersOfSize - 1000; i++) {
+            powerSet4.put(Integer.toString(i));
+        }
+    }
+
+    @Test
+    public void setUpTest() {
+        System.out.println("setUp");
+        long start = System.currentTimeMillis();
+        setUp();
+        long end = System.currentTimeMillis();
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
     }
 
     @Test
     public void size() {
+        System.out.println("size");
+        setUp();
+        long start = System.currentTimeMillis();
+        int size = powerSet.size();
+        long end = System.currentTimeMillis();
+        long time = end - start;
+        assertEquals(18000, size);
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
     }
 
     @Test
     public void put() {
+        System.out.println("put");
+        setUp();
+        long start = System.currentTimeMillis();
+        powerSet.put("asd");
+        long end = System.currentTimeMillis();
+        assertEquals(18001, powerSet.size());
+        int asd = powerSet.find("asd");
+        assertEquals("asd", powerSet.slots[asd]);
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
     }
 
     @Test
     public void get() {
+        System.out.println("get");
+        setUp();
+        long start = System.currentTimeMillis();
+        boolean get = powerSet.get("5000");
+        long end = System.currentTimeMillis();
+        assertEquals(18000, powerSet.size());
+        assertTrue(get);
+        boolean notGet = powerSet.get("sadasdad");
+        assertFalse(notGet);
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
     }
 
     @Test
     public void remove() {
+        System.out.println("remove");
+        setUp();
+        long start = System.currentTimeMillis();
+        boolean remove = powerSet.remove("5000");
+        long end = System.currentTimeMillis();
+        assertEquals(17999, powerSet.size());
+        assertTrue(remove);
+        boolean remove2 = powerSet.remove("5000");
+        assertFalse(remove2);
+        boolean remove3 = powerSet.remove("fadsf");
+        assertFalse(remove3);
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
     }
 
     @Test
     public void intersection() {
+        System.out.println("intersection");
+        setUp();
+        setUp3();
+        long start = System.currentTimeMillis();
+        PowerSet res = powerSet.intersection(powerSet3);
+        long end = System.currentTimeMillis();
+        assertEquals(1000, res.size());
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
+    }
+
+    @Test
+    public void intersection2() {
+        System.out.println("intersection2");
+        setUp();
+        setUp2();
+        long start = System.currentTimeMillis();
+        PowerSet res = powerSet.intersection(powerSet2);
+        long end = System.currentTimeMillis();
+        assertEquals(0, res.size());
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
     }
 
     @Test
     public void union() {
+        System.out.println("union");
+        setUp();
+        setUp3();
+        long start = System.currentTimeMillis();
+        PowerSet res = powerSet.union(powerSet3);
+        long end = System.currentTimeMillis();
+        assertEquals(28000, res.size());
+        long time = end - start;
+        System.out.println("Benchmarks time = " + time);
+        assertTrue(time <= 2000);
+    }
+
+    @Test
+    public void union2() {
+        System.out.println("union2");
+        setUp();
+        setUp2();
+        long start = System.currentTimeMillis();
+        PowerSet res = powerSet.union(powerSet2);
+        long end = System.currentTimeMillis();
+        assertEquals(33000, res.size());
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
     }
 
     @Test
     public void difference() {
+        System.out.println("difference");
+        setUp();
+        setUp2();
+        long start = System.currentTimeMillis();
+        PowerSet res = powerSet.difference(powerSet2);
+        long end = System.currentTimeMillis();
+        assertEquals(18000, res.size());
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
+    }
+
+    @Test
+    public void difference2() {
+        System.out.println("difference2");
+        setUp();
+        setUp3();
+        long start = System.currentTimeMillis();
+        PowerSet res = powerSet.difference(powerSet3);
+        long end = System.currentTimeMillis();
+        assertEquals(17000, res.size());
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
     }
 
     @Test
     public void isSubset() {
+        System.out.println("isSubset");
+        setUp();
+        setUp3();
+        long start = System.currentTimeMillis();
+        boolean res = powerSet.isSubset(powerSet3);
+        long end = System.currentTimeMillis();
+        assertFalse(res);
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
+    }
+
+    @Test
+    public void isSubset2() {
+        System.out.println("isSubset2");
+        setUp();
+        setUp2();
+        long start = System.currentTimeMillis();
+        boolean res = powerSet.isSubset(powerSet2);
+        long end = System.currentTimeMillis();
+        assertFalse(res);
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
+    }
+
+    @Test
+    public void isSubset3() {
+        System.out.println("isSubset3");
+        setUp();
+        setUp4();
+        long start = System.currentTimeMillis();
+        boolean res = powerSet.isSubset(powerSet4);
+        long end = System.currentTimeMillis();
+        assertTrue(res);
+        long time = end - start;
+        assertTrue(time <= 2000);
+        System.out.println("Benchmarks time = " + time);
     }
 }
