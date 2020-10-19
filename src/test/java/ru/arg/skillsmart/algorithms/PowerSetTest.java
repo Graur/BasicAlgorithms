a359baf81113f009d1bb8a26a5c8cceea50b3daf
Graur <ancongr@yandex.ru>
2020-10-19 19:18:45 +0300

    Task 10: HashSet (template)

---
+++ src/test/java/ru/arg/skillsmart/algorithms/PowerSetTest.java
@@ -0,0 +1,47 @@
+package ru.arg.skillsmart.algorithms;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class PowerSetTest {
+
+    public void setUp() {
+    }
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void put() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void intersection() {
+    }
+
+    @Test
+    public void union() {
+    }
+
+    @Test
+    public void difference() {
+    }
+
+    @Test
+    public void isSubset() {
+    }
+}
