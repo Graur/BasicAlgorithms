4ae4b2c11503784d04dedb07627c6a267d863727
Graur <ancongr@yandex.ru>
2020-10-23 20:12:27 +0300

    Task 10: HashSet (fix remove method)

--- src/main/java/ru/arg/skillsmart/algorithms/PowerSet.java
+++ src/main/java/ru/arg/skillsmart/algorithms/PowerSet.java
@@ -1,205 +1,64 @@
 package ru.arg.skillsmart.algorithms;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
-public class PowerSet
-{
-    public int size;
-    public int step;
-    public String[] slots;
-    public int count;
+public class PowerSet {
+    ArrayList<String> powerSetList;
 
-    public PowerSet()
-    {
-        // ваша реализация хранилища
-        size = 20000;
-        step = 3;
-        slots = new String[size];
-        for(int i=0; i<size; i++) slots[i] = null;
+    public PowerSet() {
+        powerSetList = new ArrayList<>(20000);
     }
 
-    public PowerSet(int sz)
-    {
-        // ваша реализация хранилища
-        size = sz;
-        step = 3;
-        slots = new String[size];
-        for(int i=0; i<size; i++) slots[i] = null;
+    public int size() {
+        return powerSetList.size();
     }
 
-    public int size()
-    {
-        // количество элементов в множестве
-        return count;
+    public void put(String value) {
+        if (!powerSetList.contains(value)) powerSetList.add(value);
     }
 
-    public int hashFun(String value)
-    {
-        // всегда возвращает корректный индекс слота
-        int h = 0;
-        byte[] bytes = value.getBytes();
-        for (byte v : bytes) {
-            h += v;
-        }
-        return h % size;
+    public boolean get(String value) {
+        return powerSetList.contains(value);
     }
 
-    private int seekSlot(String key)
-    {
-        // находит индекс пустого слота для значения, или -1
-        int index = hashFun(key);
-        if (slots[index] == null || slots[index].equals(key)) {
-            return index;
-        } else {
-            int cnt = 0;
-            int newIndex = index + step >= size ? step - (size - index) : index + step;
-            while (cnt != size) {
-                if (slots[newIndex] == null) {
-                    return newIndex;
-                } else {
-                    newIndex = newIndex + step >= size ? step - (size - newIndex) : newIndex + step;
-                    cnt++;
-                }
-            }
-        }
-        return -1;
+    public String find(int i) {
+        return powerSetList.get(i);
     }
 
-    public void put(String value)
-    {
-        // всегда срабатывает
-        if (get(value)) {
-            return;
-        }
-        int index = seekSlot(value);
-        if (index != -1) {
-            slots[index] = value;
-            count++;
-        }
+    public boolean remove(String value) {
+        return powerSetList.remove(value);
     }
 
-    public boolean get(String value)
-    {
-        // возвращает true если value имеется в множестве,
-        // иначе false
-        int index = hashFun(value);
-        if (slots[index] == null) {
-            return false;
-        } else if (slots[index].equals(value)) {
-            return true;
-        } else {
-            int count = 0;
-            int newIndex = index + step >= size ? step - (size - index) : index + step;
-            while (count != size) {
-                if (slots[newIndex] != null && slots[newIndex].equals(value))  {
-                    return true;
-                } else {
-                    newIndex = newIndex + step >= size ? step - (size - newIndex) : newIndex + step;
-                    count++;
-                }
-            }
-        }
-        return false;
-    }
-
-    public int find(String value)
-    {
-        if (value == null) {
-            return -1;
-        }
-        int index = hashFun(value);
-        if (slots[index].equals(value)) {
-            return index;
-        } else {
-            int count = 0;
-            int newIndex = index + step >= size ? step - (size - index) : index + step;
-            while (count != size) {
-                if (slots[newIndex] != null && slots[newIndex].equals(value))  {
-                    return newIndex;
-                } else {
-                    newIndex = newIndex + step >= size ? step - (size - newIndex) : newIndex + step;
-                    count++;
-                }
-            }
-        }
-        return -1;
-    }
-
-    public boolean remove(String value)
-    {
-        // возвращает true если value удалено
-        // иначе false
-        if (get(value)) {
-            int slot = hashFun(value);
-            for (int i = 0; i < size; i++) {
-                if (slots[slot] != null && slots[slot].equals(value)) {
-                    slots[slot] = null;
-                    count--;
-                    return true;
-                }
-                slot = slot + step;
-                if (slot >= size) {
-                    slot = slot - size;
-                }
-            }
-        }
-        return false;
-    }
-
-    public PowerSet intersection(PowerSet set2)
-    {
-        // пересечение текущего множества и set2
-        if (set2 == null) {
-            return new PowerSet();
-        }
+    public PowerSet intersection(PowerSet set2) {
         PowerSet result = new PowerSet();
-        for (int i = 0; i < set2.size; i++) {
-            if (set2.slots[i] != null && get(set2.slots[i])) {
-                result.put(set2.slots[i]);
-            }
+        for (String i : powerSetList) {
+            if (set2.get(i)) result.put(i);
         }
         return result;
     }
 
-    public PowerSet union(PowerSet set2)
-    {
-        // объединение текущего множества и set2
-        PowerSet result = new PowerSet(size() + set2.size());
-        for (int i = 0; i < size; i++) {
-            if (slots[i] != null) {
-                result.put(slots[i]);
-            }
-        }
-
-        for (int i = 0; i < set2.size; i++) {
-            if (set2.slots[i] != null) {
-                result.put(set2.slots[i]);
-            }
-        }
-        return result;
-    }
-
-    public PowerSet difference(PowerSet set2)
-    {
-        // разница текущего множества и set2
+    public PowerSet union(PowerSet set2) {
         PowerSet result = new PowerSet();
-        for (int i = 0; i < size; i++) {
-            if (slots[i] != null && !set2.get(slots[i])) {
-                result.put(slots[i]);
-            }
+        for (String i : powerSetList) {
+            result.put(i);
+        }
+        for (int i = 0; i < set2.size(); i++) {
+            result.put(set2.find(i));
         }
         return result;
     }
 
-    public boolean isSubset(PowerSet set2)
-    {
-        // возвращает true, если set2 есть
-        // подмножество текущего множества,
-        // иначе false
-        for (int i = 0; i < set2.size; i++) {
-            if (set2.slots[i] != null && !get(set2.slots[i])) {
-                return false;
-            }
+    public PowerSet difference(PowerSet set2) {
+        PowerSet result = new PowerSet();
+        for (String i : powerSetList) {
+            if (!set2.get(i)) result.put(i);
+        }
+        return result;
+    }
+
+    public boolean isSubset(PowerSet set2) {
+        for (int i = 0; i < set2.size(); i++) {
+            if(!powerSetList.contains(set2.find(i))) return false;
         }
         return true;
     }
