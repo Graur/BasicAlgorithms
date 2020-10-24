dc94bd3461b8732df5f9983f8e3aad61616d6a58
Graur <ancongr@yandex.ru>
2020-10-24 15:45:17 +0300

    Task 11: Bloom filter (done)

--- src/main/java/ru/arg/skillsmart/algorithms/BloomFilter.java
+++ src/main/java/ru/arg/skillsmart/algorithms/BloomFilter.java
@@ -3,39 +3,52 @@ package ru.arg.skillsmart.algorithms;
 public class BloomFilter
 {
     public int filter_len;
+    // 00000000000000000000000000000000
+    public int bitArray;
 
     public BloomFilter(int f_len)
     {
         filter_len = f_len;
         // создаём битовый массив длиной f_len ...
+        bitArray = 0;
     }
 
     // хэш-функции
     public int hash1(String str1)
     {
         // 17
+        int result = 0;
         for(int i=0; i<str1.length(); i++)
         {
-            int code = (int)str1.charAt(i);
+            int code = str1.charAt(i);
+            result = (result * 17 + code) % filter_len;
         }
         // реализация ...
-        return 0;
+        return 1 << result;
     }
     public int hash2(String str1)
     {
         // 223
         // реализация ...
-        return 0;
+        int result = 0;
+        for(int i=0; i<str1.length(); i++)
+        {
+            int code = str1.charAt(i);
+            result = (result * 223 + code) % filter_len;
+        }
+        return 1 << result;
     }
 
     public void add(String str1)
     {
         // добавляем строку str1 в фильтр
+        bitArray = bitArray | hash1(str1) | hash2(str1);
     }
 
     public boolean isValue(String str1)
     {
         // проверка, имеется ли строка str1 в фильтре
-        return false;
+        int bitMask = hash1(str1) | hash2(str1);
+        return bitMask ==  (bitArray & bitMask);
     }
 }
--- src/test/java/ru/arg/skillsmart/algorithms/BloomFilterTest.java
+++ src/test/java/ru/arg/skillsmart/algorithms/BloomFilterTest.java
@@ -2,28 +2,77 @@ package ru.arg.skillsmart.algorithms;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 
+@RunWith(JUnit4.class)
 public class BloomFilterTest {
-
-    @Before
-    public void setUp() throws Exception {
-    }
+    private String string0 = "0123456789";
+    private String string1 = "1234567890";
+    private String string2 = "2345678901";
+    private String string3 = "3456789012";
+    private String string4 = "4567890123";
+    private String string5 = "5678901234";
+    private String string6 = "6789012345";
+    private String string7 = "7890123456";
+    private String string8 = "8901234567";
+    private String string9 = "9012345678";
 
     @Test
     public void hash1() {
+        BloomFilter bf = new BloomFilter(32);
+        System.out.println(Integer.toBinaryString(bf.hash1(string0)));
+        System.out.println(Integer.toBinaryString(bf.hash1(string1)));
+        System.out.println(Integer.toBinaryString(bf.hash1(string2)));
+        System.out.println(Integer.toBinaryString(bf.hash1(string3)));
+        System.out.println(Integer.toBinaryString(bf.hash1(string4)));
+        System.out.println(Integer.toBinaryString(bf.hash1(string5)));
+        System.out.println(Integer.toBinaryString(bf.hash1(string6)));
+        System.out.println(Integer.toBinaryString(bf.hash1(string7)));
+        System.out.println(Integer.toBinaryString(bf.hash1(string8)));
+        System.out.println(Integer.toBinaryString(bf.hash1(string9)));
     }
 
     @Test
     public void hash2() {
+        BloomFilter bf = new BloomFilter(32);
+        System.out.println(Integer.toBinaryString(bf.hash2(string0)));
+        System.out.println(Integer.toBinaryString(bf.hash2(string1)));
+        System.out.println(Integer.toBinaryString(bf.hash2(string2)));
+        System.out.println(Integer.toBinaryString(bf.hash2(string3)));
+        System.out.println(Integer.toBinaryString(bf.hash2(string4)));
+        System.out.println(Integer.toBinaryString(bf.hash2(string5)));
+        System.out.println(Integer.toBinaryString(bf.hash2(string6)));
+        System.out.println(Integer.toBinaryString(bf.hash2(string7)));
+        System.out.println(Integer.toBinaryString(bf.hash2(string8)));
+        System.out.println(Integer.toBinaryString(bf.hash2(string9)));
     }
 
     @Test
     public void add() {
+        BloomFilter bf = new BloomFilter(32);
+        bf.add(string0);
+        bf.add(string1);
+        bf.add(string2);
     }
 
     @Test
     public void isValue() {
+        BloomFilter bf = new BloomFilter(32);
+        bf.add(string0);
+        bf.add(string1);
+        bf.add(string2);
+        bf.add(string3);
+        bf.add(string4);
+        bf.add(string5);
+        assertTrue(bf.isValue(string0));
+        assertTrue(bf.isValue(string1));
+        assertTrue(bf.isValue(string2));
+        assertTrue(bf.isValue(string3));
+        assertTrue(bf.isValue(string4));
+        assertTrue(bf.isValue(string5));
+        assertFalse(bf.isValue("dsfasdfasdf"));
     }
 }
