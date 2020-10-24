1d4af4558f95588de65b2ecf3f1c53c308400f0c
Graur <ancongr@yandex.ru>
2020-10-24 14:58:26 +0300

    Task 11: Bloom filter (template)

---
+++ src/test/java/ru/arg/skillsmart/algorithms/BloomFilterTest.java
@@ -0,0 +1,29 @@
+package ru.arg.skillsmart.algorithms;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BloomFilterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void hash1() {
+    }
+
+    @Test
+    public void hash2() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void isValue() {
+    }
+}
